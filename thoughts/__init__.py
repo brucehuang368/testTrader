0e5c4d3d47f4559a805e8c24f0456e57b74bd90a
bill <admin@kitsoft.net>
2021-08-08 17:01:57 +0800

    modify

--- thoughts/__init__.py
+++ thoughts/__init__.py
@@ -1,25 +1,4 @@
 # coding=utf-8
-# import traders.qianyuan.thoughts.shuangjunxian
-# import traders.qianyuan.thoughts.simple1
-# import traders.qianyuan.thoughts.simple2
-# import traders.qianyuan.thoughts.simple3
-# import traders.qianyuan.thoughts.online
-# import traders.qianyuan.thoughts.baseclass
-# import traders.qianyuan.thoughts.test
-
-from . import shuangjunxian
-from . import simple1
-from . import simple2
-from . import simple3
-from . import online
-from . import baseclass
+# 导入文件
 from . import test
-
-# Strategy = baseclass.BaseStrategy
-#
-# Strategy = shuangjunxian.TestStrategy
-# Strategy = online.TestStrategy
-
-# test
-# Strategy = test.DoubleMaStrategy
-# Strategy = test.SingleMaStrategy
+from . import demo
